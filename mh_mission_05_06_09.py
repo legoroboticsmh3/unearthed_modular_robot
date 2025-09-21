9aaa41ffda94cdd624704e99f904f600e782ba5a
pc2 <mhfll.pc1@gmail.com>
2025-09-20 20:18:03 -0700

    [Sat-09-20 20:18] Shrujana - Added code for mission 9

--- mh_mission_05_06_09.py
+++ mh_mission_05_06_09.py
@@ -10,21 +10,26 @@ R_Motor = Motor(Port.E,positive_direction=Direction.COUNTERCLOCKWISE, gears=[28,
 L_Motor = Motor(Port.F,positive_direction=Direction.CLOCKWISE, gears=[28, 20])
 Drive= DriveBase(L_Motor,R_Motor,62.4,110)
 Drive.use_gyro(True)
+
 Drive.settings(turn_rate=160)
 # Attach_Right=Motor(Port.D)
 # Attach_Left=Motor(Port.C)
-Drive.straight(680)
+Drive.straight(680)  
 Drive.turn(49)
 Drive.straight(35)
-Drive.settings(turn_rate=75)
-Drive.turn(-39)
+Drive.settings(turn_rate=75)   #to get the boulders down 
+Drive.turn(-39)        #to get the boulders down Mission number 6
 Drive.settings(turn_rate=160)
 Drive.straight(50)
-Drive.turn(-96)
-Drive.straight(-260)
+Drive.turn(-98)     #mission number 5
+Drive.straight(-260)   #push the boulders back
 # Drive.straight(590)
 Drive.straight(200)
 Drive.turn(8)
-Drive.straight(390)
-Drive.turn(38)
-Drive.straight(-200)
+Drive.straight(410)
+Drive.turn(35)
+Drive.settings(straight_speed=200)
+Drive.straight(-220)
+Drive.turn(-15)
+Drive.straight(100)
+
