e788cc0cef7fdb0758df11e21484970c36fb182e
pc2 <mhfll.pc1@gmail.com>
2025-09-21 17:59:10 -0700

    [Sun-09-21 17:59] Bhaskar - Still working on mission 9, very hard to tip the scale up. need minor arrangements

--- mh_mission_05_06_09.py
+++ mh_mission_05_06_09.py
@@ -28,8 +28,27 @@ Drive.straight(200)
 Drive.turn(8)
 Drive.straight(410)
 Drive.turn(35)
-Drive.settings(straight_speed=200)
-Drive.straight(-220)
-Drive.turn(-15)
-Drive.straight(100)
+Drive.settings(straight_speed=800)
+Drive.straight(-250)
+wait(1000)
+Drive.straight(200)
+
+
+
+
+#Drive.settings(straight_speed=200)
+
+#Drive.straight(-220)
+#Drive.turn(10)
+#Drive.straight(-20)
+#wait(2000)
+#Drive.stop()
+
+#Drive.turn(-15)
+#Drive.straight(100, then=Stop.Hold)
+#Drive.straight(-10)
+#wait(1000)
+#Drive.straight(50)
+#Drive.turn(15)
+
 
