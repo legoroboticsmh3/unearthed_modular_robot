a417fd37c4e598c690bdcdbe9d9fba17a2a55362
legoroboticsmh3 <legoroboticsmh3@gmail.com>
2025-10-05 18:40:45 -0700

    [2025-10-05 18:40] Ritwij - The working code said it is not going to work then the coahes and Ritwij somewhat redid it. The missions are being half completed.

--- mh_mission_03_04 copy.py
+++ mh_mission_03_04 copy.py
@@ -5,6 +5,7 @@ from pybricks.robotics import DriveBase
 from pybricks.tools import wait, StopWatch
 
 
+#these are all the default setings for the robot
 hub = PrimeHub()
 R_motor=Motor(Port.E,Direction.COUNTERCLOCKWISE, gears=[28,20])
 L_motor=Motor(Port.F,Direction.CLOCKWISE, gears=[28,20])
@@ -12,31 +13,37 @@ Left_attach=Motor(Port.A, gears=[28, 36])
 Right_attach=Motor(Port.B, gears=[28, 36])
 Drive=DriveBase(L_motor, R_motor, 62.4, 110)
 
-# THE RED NEXT TO THE LINE NUMBER MEANS IT HAS TO BE CHANGED IF NOT THERE EVERYTHING IS FINE TO REMOVE IT PRESS IT.
-Drive.use_gyro(True)
+#going to the mission model mission 3
+Drive.straight(-950)
+Drive.turn(-95)
+Left_attach.run_angle(75, 100)
+Drive.straight(250)
+
+
+"""Drive.use_gyro(True)
+speed=1000
 Left_attach.run_angle(200, -15)
-#tuoched the wall and turned to face the model
 Drive.straight(-980)
 Drive.turn(-91)
-#put down the attachment to grab the model
 Right_attach.run_angle(150,100)
-Left_attach.run_angle(240,117.6)
 Drive.settings(straight_speed=50)
-Drive.straight(122)
+Left_attach.run_angle(240,112.6)
 Left_attach.run_angle(200,-12)
+Drive.straight(122)
 wait(500)
-Right_attach.run_angle(100,-130)
+Right_attach.run_angle(100,-120)
 Drive.straight(-145)
 Drive.settings(straight_speed=500)
 Drive.turn(90)
-#grabbed the model 
 wait(500)
-#goes to the mission 13
 Drive.turn(-48)
-Left_attach.run_angle(500, 80)
+wait(100)
+Left_attach.run_angle(500,90)
 Drive.settings(straight_speed=100)
-Drive.straight(363.5)
+Drive.straight(378.7)
+wait(100)
 Drive.settings(straight_speed=1000)
 Left_attach.dc(-9000)
 wait(600) 
-#missions completed
+Left_attach.run_angle(500,300)
+"""
