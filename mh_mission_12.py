c0769f1c086dd7701bc8ac07551a73ae02daed66
legoroboticsmh3 <legoroboticsmh3@gmail.com>
2025-11-01 13:18:16 -0700

    Bhumi on mission_12 run_4

--- mh_mission_12.py
+++ mh_mission_12.py
@@ -10,22 +10,37 @@ from pybricks.tools import wait
 
 #going to the mission model mission 3
 async def run_4(robot):
-    robot.Drive.settings(straight_speed=350)
+    robot.Drive.settings(straight_speed=650)
     robot.Drive.settings(turn_rate=160)
-    await robot.Drive.straight(470)
+
+    await robot.Drive.straight(550)
     #await robot.Right_attach.run_angle(200, 130*-1)
+    #right arm down to grab
     robot.Right_attach.dc(-75)
     await wait(250)
+
+    # drive back and lift right arm up
     robot.Right_attach.stop()
     await robot.Drive.straight(-120)
     await robot.Right_attach.run_angle(200, -130*-1)
-    await robot.Drive.turn(-90)
+
+    # drive straight to lift ship up
+    await robot.Drive.straight(270)
+
+    # deliver
+    await robot.Left_attach.run_angle(200, 70*-0.77)
+    await robot.Left_attach.run_angle(200, -70*-0.77)
+    # drive back home
+    await robot.Drive.straight(-550)
+
+'''   await robot.Drive.turn(-90)
     await robot.Drive.straight(182)
     await robot.Drive.turn(90)
     await robot.Right_attach.run_angle(200, 130*-1)
     await robot.Drive.straight(-60)
-    await robot.Drive.straight(370)
-    await robot.Drive.straight(-600)
+    await robot.Drive.straight(360)'''
+
+ 
 
 '''
     robot.Drive.settings(straight_speed=350)
