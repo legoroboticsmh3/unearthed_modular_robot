0baf8caeccaa7a7221cee3698ceb405d64f4a0ec
legoroboticsmh3 <legoroboticsmh3@gmail.com>
2025-11-05 17:10:38 -0800

    adding 3_13

---
+++ mh_mission_03_13.py
@@ -0,0 +1,47 @@
+from pybricks.tools import wait
+
+async def run_6(robot):
+    print("Starting Run: " + __name__)
+    robot.Drive.settings(straight_speed=400)
+    robot.Drive.settings(turn_rate=100)
+    
+    #Drives to mission 03
+    await robot.Drive.straight(660)
+    await robot.Drive.turn(41)
+    await wait(500)
+
+    #Drives and get ready position to lift the missions 03 
+    await robot.Left_attach.run_angle(100,20)
+    await wait(100)
+    await robot.Right_attach.run_angle(100,-120)
+    await wait(100)
+    await robot.Drive.straight(240)
+    await robot.Drive.turn(-14.5)
+    await wait(500)
+
+    
+
+    #Lifts up mission 03
+    await robot.Right_attach.run_angle(50,133)
+    await wait(500)
+
+
+    #Drivies to mission 13
+    robot.Drive.settings(straight_speed=100)
+    await robot.Drive.straight(-50)
+    await robot.Drive.turn(77.5)
+    await wait(100)
+    await robot.Right_attach.run_angle(100,-132)
+    await wait(200)
+    await robot.Drive.straight(139)
+
+
+    #lifts up mission 13
+    await robot.Right_attach.run_angle(400,133)
+    robot.Drive.settings(straight_speed=400)
+
+    
+    #drives back home
+    await robot.Drive.straight(-350)
+    await robot.Drive.turn(77)
+    await robot.Drive.straight(660)
