d5caf04d5538c36666b3a8c4fa482bc89ea8eaa1
legoroboticsmh3 <legoroboticsmh3@gmail.com>
2025-12-19 15:35:18 -0800

    Completed missions 5, 6, 8, and 9 successfully in 24 seconds - Anay

--- mh_mission_08_05_06_09.py
+++ mh_mission_08_05_06_09.py
@@ -1,7 +1,9 @@
-from pybricks.tools import wait
+from pybricks.tools import wait, multitask
 
 async def run_6(robot):
   print("Starting Run: " + __name__)
+
+  await multitask(robot.Right_attach.run_angle(500, 50), wait(700), race=True)
 
   robot.Drive.settings(turn_rate=160)
   await robot.Drive.straight(420)
@@ -28,23 +30,29 @@ async def run_6(robot):
   await robot.Drive.straight(50)
   await robot.Drive.turn(45)
   await robot.Drive.straight(220) #reach the Boulder station
-  #await robot.Drive.turn(-15)
-  await robot.Drive.straight(15)
+  await robot.Drive.turn(-3)
+  await robot.Drive.straight(16)
   await robot.Right_attach.run_angle(500,-85) #Complete the Forge
+  await robot.Right_attach.run_angle(500,50)
+  await robot.Right_attach.run_angle(500,-50)
   await robot.Drive.turn(-15)
   await robot.Right_attach.run_angle(500,85)
   await robot.Drive.turn(-35)
   await robot.Drive.straight(140)
-  await wait(500)
-  await robot.Right_attach.run_angle(500,-50)
+  await robot.Right_attach.run_angle(500,-60)
+  await wait(700)
   robot.Drive.settings(turn_rate=100)
   await robot.Drive.turn(42)
   robot.Drive.settings(turn_rate=160)
-  await wait(500)
   await robot.Right_attach.run_angle(500,-15)
+  await wait(500)
   await robot.Right_attach.run_angle(500,60)
   await robot.Drive.turn(-10)
-  await robot.Drive.straight(-320)
-  await robot.Drive.turn(-95)
-  await robot.Right_attach.run_angle(500,-65)
+  await robot.Drive.straight(-280)
+  await robot.Drive.turn(-90)
+  await robot.Drive.straight(20)
+  await robot.Right_attach.run_angle(500,-75)
+  await wait(500)
   await robot.Drive.straight(80)
+  await robot.Drive.turn(-90)
+  await robot.Drive.straight(275)
