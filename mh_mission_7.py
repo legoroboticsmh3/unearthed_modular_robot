f140f4a110d917a8d1bf5c362e55d2ac92601666
pc2 <mhfll.pc1@gmail.com>
2026-01-18 17:06:26 -0800

    Completed mission 7 Heavy Lifting

--- mh_mission_7.py
+++ mh_mission_7.py
@@ -11,22 +11,23 @@ async def run_5(robot):
     await multitask(robot.Right_attach.run_angle(100,90), wait(500), race=True)
     robot.Drive.settings(turn_rate=75)
     await robot.Drive.straight(15)
-    await robot.Drive.turn(-11)
+    await robot.Drive.turn(-11) #turn to avoid hitting silo
     await robot.Drive.straight(740)  #reach Heavy Lifting
     await robot.Drive.turn(51) #face heavy lifting
     #await robot.Drive.straight(-40)  
-    await robot.Drive.straight(40)    #reach heavy lifting
-    await robot.Right_attach.run_angle(150,-150)
+    await robot.Drive.straight(60)    #reach heavy lifting
+    await robot.Right_attach.run_angle(150,-150) #placing attachment down to pick up the heavy lifting
     await wait(900)
     robot.Drive.settings(straight_speed=200)
-    await robot.Drive.straight(20) #couple with the ring of the mill
+    await robot.Drive.straight(50) #couple with the ring of the mill
     await wait(300)
-    await robot.Right_attach.run_angle(200,150)
+    await robot.Right_attach.run_angle(200,150) #pick up the weight
     await wait(200)
-    await robot.Drive.straight(-80)
-    await robot.Drive.turn(-36)
+    robot.Drive.settings(straight_speed=600)
+    await robot.Drive.straight(-80) #come out of the area
+    await robot.Drive.turn(-50)#turn towards blue base
     robot.Drive.settings(turn_rate=160)
-    await robot.Drive.straight(-780)
+    await robot.Drive.straight(-780) #going to blue base
     
 
 #####Below is the original code for mission 7 heavy lifting with the originl starting alignment of 7T"
