0ade1d3e93493748c1f402644635c7b40224121a
pc2 <mhfll.pc1@gmail.com>
2026-02-08 18:48:58 -0800

    Minor Tweaks

--- mh_mission_7.py
+++ mh_mission_7.py
@@ -13,48 +13,17 @@ async def run_5(robot):
     await robot.Drive.straight(15)
     await robot.Drive.turn(-11) #turn to avoid hitting silo
     await robot.Drive.straight(740)  #reach Heavy Lifting
-    await robot.Drive.turn(54) #face heavy lifting was 51
-    #await robot.Drive.straight(-40)  
+    await robot.Drive.turn(54) #face heavy lifting
     await robot.Drive.straight(60)    #reach heavy lifting
     await robot.Right_attach.run_angle(150,-180) #placing attachment down to pick up the heavy lifting
     await wait(900)
     robot.Drive.settings(straight_speed=200)
     await robot.Drive.straight(50) #couple with the ring of the mill
     await wait(300)
-    #await robot.Right_attach.run_angle(200,150) #pick up the weight
-    robot.Right_attach.dc(100)
+    robot.Right_attach.dc(80)
     await wait(2000)
     robot.Drive.settings(straight_speed=600)
     await robot.Drive.straight(-80) #come out of the area
     await robot.Drive.turn(-50)#turn towards blue base
     robot.Drive.settings(turn_rate=160)
     await robot.Drive.straight(-800) #going to blue base
-    
-
-#####Below is the original code for mission 7 heavy lifting with the originl starting alignment of 7T"
-    # robot.Right_attach.run_angle(100,-90*-1,then=Stop.HOLD)
-    # robot.Drive.settings(straight_speed=800)
-    # robot.Drive.settings(turn_rate=75)
-    # await robot.Drive.straight(770)  #reach Heavy Lifting
-
-
-
-
-    # await robot.Drive.turn(35)
-    # await robot.Drive.straight(-40)  
-    # await robot.Drive.straight(10)    #reach heavy lifting
-    # await robot.Right_attach.run_angle(200,80*-1)
-    # robot.Drive.settings(straight_speed=200)
-    # await robot.Drive.straight(63) #couple with the ring of the mill
-    # await robot.Right_attach.run_angle(180,-75*-1)
-    # await robot.Drive.straight(-80)
-    # await robot.Drive.turn(-44)
-    # robot.Drive.settings(straight_speed=500)
-    # robot.Drive.settings(turn_rate=160)
-    
-    # await robot.Drive.straight(-813)
-    
-
-
-
-
